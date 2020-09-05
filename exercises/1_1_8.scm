ff2e3c45b64d8c875ae30f5f51341748f2a0391b
Mario Balan <mario.balan@gmail.com>
2020-09-04 23:13:45 -0300

    add sicp exercise 1.1.8

---
+++ exercises/1_1_8.scm
@@ -0,0 +1,30 @@
+#lang scheme
+
+(define (cbrt-iter guess x)
+    (if (good-enough? guess x)
+          guess
+	        (cbrt-iter (improve guess x) x)))
+
+; searching for other answers to 1.1.7 I discovered that:
+; if you iterate until guess and next guess are equal
+; you can get an answer to the limit of system precision.
+; like this:
+(define (good-enough? guess x)
+    (= (improve guess x) guess))
+
+;this is the only procedure that needs changing
+;the 'average' procedure is discarded
+(define (improve guess x)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+  
+(define (square x) (* x x))
+  
+(define (cbrt x)
+    (cbrt-iter 1.0 x))
+
+; for heliping with tests:
+(define (cube x)(* x x x))
+
+;Tests:
+(cbrt 8)
+(cbrt (cube 10))
